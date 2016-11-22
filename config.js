50c806ece8cdf850988c43ae8a963b2e2b922821
bhartijain <bhartij@infratab.in>
2016-11-22 19:36:32 +0530

    Setup marionette

---
+++ config.js
@@ -0,0 +1,12 @@
+requirejs.config({
+  paths: {
+    jquery: 'lib/jquery',
+    underscore: 'lib/underscore',
+    backbone: 'lib/backbone',
+    backboneRadio : 'lib/backbone.radio',
+    marionette : 'lib/backbone.marionette.min',
+    handlebar : 'lib/handlebars-v4.0.5'  
+  }
+});
+
+require(['app']);
